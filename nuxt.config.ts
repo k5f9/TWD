431eb79543d6931f8d64fcc143ebd00944207fe2
k5f9d <122154388+k5f9@users.noreply.github.com>
2025-01-18 15:54:05 +0700

    nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
+  server: {
+    port: process.env.PORT || 3000, // Используем PORT из окружения или 3000 по умолчанию
+    host: '0.0.0.0', // Хост должен быть 0.0.0.0, чтобы Railway мог подключиться
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
 
-})
+}
