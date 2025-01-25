4aeb4a9ee61995ae6d7eb56ed8689ce496e3ebbf
k5f9d <122154388+k5f9@users.noreply.github.com>
2025-01-25 19:58:29 +0700

    Update Data-Weather-Time.vue

--- components/Data-Weather-Time.vue
+++ components/Data-Weather-Time.vue
@@ -11,11 +11,15 @@
             <div
                 :class="{
                     'opacity-0 scale-y-0 h-0 mt-0': !state.isTimeVisible,
-                    'opacity-100 scale-y-100 h-[394px] mt-5': state.isTimeVisible,
+                    'opacity-100 scale-y-100 h-[200px] mt-5': state.isTimeVisible,
                 }"
                 class="bg-[#f2fde7] transition-all duration-500 w-[300px] rounded-[15px]"
             >
-                <p id="pt-1" class='px-[102px] py-[150px] font-bold text-2xl'></p>
+            <div class="py-[60px]">
+                <p id="pt-1" class='px-[102px] font-bold text-2xl'></p>
+                <p id="pt-2" class='px-[105px] font-medium opacity-60 text-1xl' >UTC+7</p>
+            </div>
+                
             </div>
         </div>
 
@@ -23,18 +27,18 @@
         <div class="flex flex-col items-center">
             <button
                 class="bg-[#B0BC98] text-[40px] font-bold w-[300px] h-[56px] item-center rounded-[15px] transform transition-transform duration-200 active:scale-90 hover:scale-105"
-                @click="toggleSlide('Weather')"
+                @click="Weather"
             >
                 Weather
             </button>
             <div
                 :class="{
                     'opacity-0 scale-y-0 h-0 mt-0': !state.isWeatherVisible,
-                    'opacity-100 scale-y-100 h-[394px] mt-5': state.isWeatherVisible,
+                    'opacity-100 scale-y-100 h-[200px] mt-5': state.isWeatherVisible,
                 }"
                 class="bg-[#e4e8dc] transition-all duration-500 w-[300px] rounded-[15px]"
             >
-                <p id="pw-1"></p>
+                <p id="pw-1" class='px-[115px] py-[70px] font-bold text-2xl' >{{ temperature }}°C</p>
             </div>
         </div>
 
@@ -49,11 +53,15 @@
             <div
                 :class="{
                     'opacity-0 scale-y-0 h-0 mt-0': !state.isDataVisible,
-                    'opacity-100 scale-y-100 h-[394px] mt-5': state.isDataVisible,
+                    'opacity-100 scale-y-100 h-[200px] mt-5': state.isDataVisible,
                 }"
                 class="bg-[#edf5e8] transition-all duration-500 w-[300px] rounded-[15px]"
             >
-                <p id="pd-1" class='px-[95px] py-[150px] font-bold text-2xl'></p>
+            <div class="py-[60px]">
+                <p id="pd-1" class='px-[84px] font-semibold text-2xl'></p>
+                <p id="pd-2" class='px-[84px] font-medium opacity-60 text-2xl'></p>
+            </div>
+                
             </div>
         </div>
     </div>
@@ -61,6 +69,24 @@
 
 <script setup lang="js">
 import { reactive } from 'vue';
+import { ref } from 'vue';
+
+const temperature = ref(null); // Температура
+
+async function Weather() {
+    state.isWeatherVisible = !state.isWeatherVisible;
+
+    try {
+        const response = await fetch('/api/weather?lat=53.3606&lon=83.7636');
+        const data = await response.json();
+
+        console.log('Данные ответа:', data);
+        temperature.value = data.fact.temp; // Установка температуры
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+}
+
 
 
 const state = reactive({
@@ -88,10 +114,52 @@ onMounted(() => {
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const seconds = date.getSeconds().toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');// Добавляем +1 к месяцу
+    let name_month;
+
+    switch(month){
+        case "01":
+            name_month = "Января";
+
+        case "02":
+            name_month = "Февраля";
+
+        case "03":
+            name_month = "Марта";
+
+        case "04":
+            name_month = "Апреля";
+
+        case "05":
+            name_month = "Мая";
+
+        case "06":
+            name_month = "Июня";
+
+        case "07":
+            name_month = "Июля";
+
+        case "08":
+            name_month = "Августа";
+        
+        case "09":
+            name_month = "Сентября";
+
+        case "10":
+            name_month = "Октября";
+
+        case "11":
+            name_month = "Ноября";
+
+        case "12":
+            name_month = "Декабря";
+
+    }
+
     const year = date.getFullYear();
     document.getElementById("pt-1").innerHTML = `${hours}:${minutes}:${seconds}`;
-    document.getElementById("pd-1").innerHTML = `${day}.${month}.${year}`;
+    document.getElementById("pd-1").innerHTML = `${day} ${name_month}`;
+    document.getElementById("pd-2").innerHTML = year;
     }, 1000);
 
     onUnmounted(() => clearInterval(timeInterval));
